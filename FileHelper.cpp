5fa5489060d3fb7ba0d6448860bc4c9b605d0827
PhucNMSE200061 <binguyenfpt1407@gmail.com>
2026-02-04 23:46:49 +0700

    Refactor: format showMainMenu function for consistency

--- FileHelper.cpp
+++ FileHelper.cpp
@@ -5,10 +5,11 @@ using namespace std;
 /*====================== SHOW MENU =========================*/
 /*==========================================================*/
 
-void showMainMenu () {
-	cout <<"1. Add Schedule" << endl;
-	cout <<"2. Update Schedule" << endl;
-	cout <<"3. Search Destination Schedule" << endl;
-	cout <<"4. Calculate total Revenue / Flight" << endl;
-	cout <<"5. Exist" << endl;
+void showMainMenu()
+{
+	cout << "1. Add Schedule" << endl;
+	cout << "2. Update Schedule" << endl;
+	cout << "3. Search Destination Schedule" << endl;
+	cout << "4. Calculate total Revenue / Flight" << endl;
+	cout << "5. Exist" << endl;
 }
